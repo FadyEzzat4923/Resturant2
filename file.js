35c8f6dfafc094ac0ca489ad60d8f82492ed3bf4
Fady_Ezzat <01024834947.f.e.e.49234923@gmail.com>
2023-05-15 01:48:06 +0200

    modify in js & css Files

--- file.js
+++ file.js
@@ -3,9 +3,9 @@ let open = document.querySelector(".dialog");
 let open_btn = document.querySelector(".hr-btn");
 let up = document.querySelector(".up");
 let bar = document.querySelector(".head-bar");
-let head_card = document.querySelector(".head-card"); 
-let icon_title = document.querySelectorAll(".icon-title"); 
-let hr_icon = document.querySelectorAll(".hr-icon"); 
+let head_card = document.querySelector(".head-card");
+let icon_title = document.querySelectorAll(".icon-title");
+let hr_icon = document.querySelectorAll(".hr-icon");
 // Program
 open.style.display = "none";
 up.style.display = "none";
@@ -32,21 +32,19 @@ window.onscroll = () => {
 
     if (window.scrollY >= 507) {
         bar.style.cssText = "position: fixed ;top:0 ; left: 6%;  background-color: rgba(255, 255, 255, 0.95); z-index : 999 ;";
-        open.style.cssText = "position : fixed; display: none;";
     }
     else {
         bar.style.position = "static";
-        open.style.cssText = "position : absolute; display: none;";
     }
 }
-window.onload = ( )=>{
+window.onload = () => {
     head_card.style.cssText = "animation: appear 1.5s 1 ;";
 }
-for( let i =0; i< icon_title.length ; i++){
-icon_title[i].onclick = ()=>{
-    hr_icon.forEach((e)=>{
-        e.style.cssText = "";
-    })
-hr_icon[i].style.cssText = "width: 100%; background-color: #B67232; height: 2px; ";
-}
+for (let i = 0; i < icon_title.length; i++) {
+    icon_title[i].onclick = () => {
+        hr_icon.forEach((e) => {
+            e.style.cssText = "";
+        })
+        hr_icon[i].style.cssText = "width: 100%; background-color: #B67232; height: 2px; ";
+    }
 }
